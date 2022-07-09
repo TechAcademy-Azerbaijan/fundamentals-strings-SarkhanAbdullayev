7fcd4b6e7fe5a698cb0dc55ad3895fb108611dc8
Sarkhan <syako44@gmail.com>
2022-07-09 04:11:48 +0400

    Update main.js

--- task-10/main.js
+++ task-10/main.js
@@ -1,9 +1,18 @@
-const prompt = require('prompt');
-prompt.start();
+let str = "sd fsdf sdfsd f fd"
+let indexes = []
 
+for( let i = 0 ; i < str.length ; i++ ){
+    if(str[i] == " "){
+        indexes.push(i);
+    }
+}
+let first = indexes[0]
+let last = indexes[indexes.length - 1]
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+if(first == undefined && last == undefined){
+    console.log("-1");
+}
+else{
+    console.log(first);
+    console.log(last);
+}

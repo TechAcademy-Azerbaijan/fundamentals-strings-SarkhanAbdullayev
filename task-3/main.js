28a69479ae514a1895e3550db5e4ea86210f3dfb
Sarkhan <syako44@gmail.com>
2022-07-09 04:06:57 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,9 +1,20 @@
-const prompt = require('prompt');
-prompt.start();
+let n = "255555222"
+let five = 0
+let two = 0
 
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+for(let el of n){
+    if( el == 5 ){
+        five++;
+    }
+    else{ two++
+    }
+}
+if( five > two ){
+    console.log("5");
+}
+else if( two > five ){
+    console.log("2");
+}
+else{
+    console.log("=");
+}
